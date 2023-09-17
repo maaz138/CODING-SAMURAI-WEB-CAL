dedcc20ca8653bd17d8e91b1e6d79a7119df2405
Maaz Hassan <maazhassan440@gmail.com>
2023-09-18 00:41:51 +0500

    first commit

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 let string = "";
 let buttons = document.querySelectorAll('.button');
 Array.from(buttons).forEach((button)=>{
@@ -17,4 +18,23 @@ button.addEventListener('click', (e)=>{
     }
     
 })
+let string = "";
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+button.addEventListener('click', (e)=>{
+    if(e.target.innerHTML == '='){
+        string = eval(string);
+        document.querySelector('input').value = string;
+    }
+    else if(e.target.innerHTML == 'C'){
+        string = " ";
+        document.querySelector('input').value = string;
+    }
+    else{
+        console.log(e.target)
+    string = string + e.target.innerHTML;
+    document.querySelector('input').value = string;
+    }
+    
 })
+})})
